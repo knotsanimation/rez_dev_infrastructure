167a0de5e990cff3fae5595d2577807609f182df
Liam Collod <lcollod@gmail.com>
2023-11-28 00:38:29 +0100

    refacto: full code refacto of the install/uninstall process
    - using functions for clarity
    - using simple logging system
        - wrote to disk for uninstall, as PATH avriable manipulation are sensitive
    - add installation of python on the user system
    - more robust

--- installing/user-install.ps1
+++ installing/user-install.ps1
@@ -1,60 +1,145 @@
-$rez_version = "2.113.0"
-$rez_url = "https://github.com/AcademySoftwareFoundation/rez/archive/refs/tags/$rez_version.zip"
-$python_install = "N:\skynet\apps\rez\python\platform-windows\arch-AMD64"
-$knots_install_path = "C:\Program Files\knots"
-$rez_full_install_path = Join-Path $knots_install_path "rez"
-$rez_scripts = Join-Path $rez_full_install_path "Scripts" "rez"
-$rez_config_file = "N:\skynet\apps\rez\config\.rezconfig"
+# global config
+$ErrorActionPreference = "Stop"
 
-if (-not (Test-Path -Path $python_install)) {
-    throw "Python directory does not exists, check your properly mapped the nas drives."
+# // Utility code
+
+function Log {
+    param ($message, $level, $color)
+    Write-Host "$level | $((Get-Date).ToString() ) [knots-install] $message" -ForegroundColor $color
 }
-$env:PATH += ";$python_install"
-# safety that will throw if there was an issue with the above
-python -V
+function LogDebug { Log @args "DEBUG" "DarkGray" }
+function LogInfo { Log @args "INFO" "White" }
+function LogSucess { Log @args "SUCCESS" "Green" }
+function LogWarning { Log @args "WARNING" "Yellow" }
+function LogError { Log @args "ERROR" "Red" }
 
-# create temporary directory for download
-$temp_directory = Join-Path $Env:Temp "rezinstall-$(New-Guid)"
-echo "creating temporary directory $temp_directory ..."
-New-Item -Type Directory -Path $temp_directory | Out-Null
-
-$rez_src = Join-Path $temp_directory "rez.zip"
-echo "downloading $rez_url to $rez_src ..."
-Invoke-WebRequest -Uri $rez_url -OutFile $rez_src
-
-echo "unzipping $rez_src ..."
-Expand-Archive $rez_src -DestinationPath $temp_directory
-# path of the directory that was extracted from the zip
-$rez_src = Join-Path $temp_directory "rez-$rez_version"
-
-if (-not (Test-Path -Path $knots_install_path)) {
-    echo "creating $knots_install_path"
-    New-Item $knots_install_path -ItemType Directory
+function Create-TmpDir {
+    param([string]$prefix)
+    $tmp_root = [System.IO.Path]::GetTempPath()
+    $tmp_directory = "$tmp_root\$prefix$( New-Guid )"
+    LogDebug "Creating temporary directory $tmp_directory"
+    New-Item -Type Directory -Path $tmp_directory | Out-Null
+    return $tmp_directory
 }
 
-if (-not (Test-Path -Path $rez_full_install_path)) {
-    echo "creating $rez_full_install_path"
-    New-Item $rez_full_install_path -ItemType Directory
+# // Installation Code
+
+function Install-Python {
+    param([string]$python_version, [string]$target_dir)
+
+    if (Test-Path -Path $python_install) {
+        LogWarning "python already installed at $python_install"
+        return
+    }
+
+    $tmp_directory = Create-TmpDir -prefix "knots-python-install-"
+    $python_tmp_dir = "$tmp_directory\python"
+
+    $nuget_url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    $nuget_path = "$tmp_directory\nuget.exe"
+
+    LogInfo "downloading nuget to $nuget_path"
+    Invoke-WebRequest $nuget_url -OutFile $nuget_path
+
+    LogInfo "downloading python $python_version to $python_tmp_dir"
+    Start-Process -NoNewWindow -Wait -FilePath $nuget_path -ArgumentList "install python -OutputDirectory $python_tmp_dir -Version $python_version"
+    $python_tmp_install_dir = "$python_tmp_dir\python.$python_version\tools"
+
+    LogInfo "installing python to $target_dir"
+    Copy-Item -Path $python_tmp_install_dir -Destination $target_dir -Recurse
+
+    LogDebug "removing temporary directory $tmp_directory"
+    Remove-Item $tmp_directory -Force -Recurse
 }
-else {
-    Remove-Item $temp_directory -Recurse -Force
-    throw "rez seems to be already installed at $rez_full_install_path"
+
+function Install-Rez {
+    param([string]$rez_version, [string]$target_dir)
+
+    if (Test-Path -Path $target_dir) {
+        LogWarning "rez already installed at $target_dir"
+        return
+    }
+
+    $rez_url = "https://github.com/AcademySoftwareFoundation/rez/archive/refs/tags/$rez_version.zip"
+
+    # create temporary directory for download
+    $temp_directory = Create-TmpDir -prefix "knots-rezinstall"
+
+    $rez_src = "$temp_directory\rez.zip"
+    LogInfo "downloading $rez_url to $rez_src ..."
+    Invoke-WebRequest -Uri $rez_url -OutFile $rez_src
+
+    LogInfo "unzipping $rez_src ..."
+    Expand-Archive $rez_src -DestinationPath $temp_directory
+    # path of the directory that was extracted from the zip
+    $rez_src = "$temp_directory\rez-$rez_version"
+
+    LogInfo "creating $target_dir"
+    New-Item $target_dir -ItemType Directory | Out-Null
+
+    $previous_cwd = (Get-Item .).FullName
+    Set-Location $rez_src
+    Start-Process python -Wait -NoNewWindow -ArgumentList "install.py --help"
+    Set-Location $previous_cwd
+
+    LogDebug "removing temporary directory $temp_directory"
+    Remove-Item $temp_directory -Force -Recurse
 }
 
-echo "creating $rez_full_install_path"
-New-Item $rez_full_install_path -ItemType Directory
+function Install-System {
+    param([string]$rez_config_file, [string]$rez_scripts)
 
-cd $rez_src
-python install.py --help
+    # query from global system as we already modified PATH for this session
+    $new_path_var = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine)
+    $new_path_var = $new_path_var + ";$rez_scripts"
 
-# query from global system as we already modified PATH for this session
-$new_path_var = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine)
-$new_path_var = $new_path_var + ";$rez_scripts"
-echo "setting environment variable PATH with $new_path_var"
-[Environment]::SetEnvironmentVariable('PATH', $new_path_var, 'Machine')
-echo "setting environment variable REZ_CONFIG_FILE with $rez_config_file"
-[Environment]::SetEnvironmentVariable('REZ_CONFIG_FILE', $rez_config_file, 'Machine')
+    LogInfo "setting environment variable PATH with $new_path_var"
+    [Environment]::SetEnvironmentVariable('PATH', $new_path_var, 'Machine')
 
-Remove-Item $temp_directory -Recurse -Force
-echo "installation finished; you can test it by typing:"
-echo "  rez -V"
+    LogInfo "setting environment variable REZ_CONFIG_FILE with $rez_config_file"
+    [Environment]::SetEnvironmentVariable('REZ_CONFIG_FILE', $rez_config_file, 'Machine')
+
+}
+
+function Start-Full-Installation {
+
+    # configuration
+    $rez_version = "2.113.0"
+    $python_version = "3.10.11"
+    $knots_install_path = "C:\Program Files\knots"
+    $python_install = "$knots_install_path\python-rez"
+    $rez_full_install_path = "$knots_install_path\rez"
+    $rez_scripts = "$rez_full_install_path\Scripts\rez"
+    # TODO update when definitive
+    $rez_config_file = "N:\skynet\apps\rez\config\.rezconfig"
+
+    # TODO uncomment
+    #if (-not (Test-Path -Path $rez_config_file)) {
+    #    throw "Rez config file does not exists, check your properly mapped the NAS drives."
+    #}
+    if (-not(Test-Path -Path $knots_install_path)) {
+        LogInfo "creating $knots_install_path"
+        New-Item -Type Directory -Path $knots_install_path | Out-Null
+    }
+
+    Install-Python -python_version $python_version -target_dir $python_install
+    LogSucess "installed python $python_version to $python_install"
+
+    $env:PATH += ";$python_install"
+
+    $check_python_path = (Get-Command python).Path
+    if (-not ($check_python_path -eq "$python_install\python.exe")) {
+        throw "Issue with python installation, unexpected path $check_python_path"
+    }
+
+    Install-Rez -rez_version $rez_version -target_dir $rez_full_install_path
+    LogSucess "installed rez $rez_version to $rez_full_install_path"
+
+    # TODO uncomment
+    # Install-System -rez_config_file $rez_config_file -rez_scripts $rez_scripts
+
+    LogSucess "installation finished; you can test it by typing:"
+    LogSucess "  rez -V"
+}
+
+Start-Full-Installation
--- installing/user-uninstall.ps1
+++ installing/user-uninstall.ps1
@@ -1,18 +1,53 @@
-$rez_install_path = "C:\Program Files\knots\rez"
-$rez_scripts_path = "C:\Program Files\knots\rez\Scripts\rez"
+$ErrorActionPreference = "Stop"
 
-# remove rez local installation
-echo "removing $rez_install_path ..."
-Remove-Item $rez_install_path -Recurse -Force
+$knots_install_path = "C:\Program Files\knots"
+$uninstall_log_path = "$knots_install_path\user-uninstall.log"
+# we need it to create log files
+if (-not(Test-Path -Path $knots_install_path)) {
+    Write-Output "creating $knots_install_path"
+    New-Item -Type Directory -Path $knots_install_path | Out-Null
+}
 
-# unset PATH environment variable
-$current_path = [System.Environment]::GetEnvironmentVariable('PATH', 'Machine')
-echo "current PATH environment variable is $current_path"
-$new_path = ($current_path.Split(';') | Where-Object { $_ -ne $rez_scripts_path }) -join ';'
-echo "new PATH environment variable is $new_path"
-[System.Environment]::SetEnvironmentVariable('PATH', $path, 'Machine')
+function Log {
+    param ($message, $level, $color)
+    $final_message = "$level | $((Get-Date).ToString() ) [knots-install] $message"
+    Write-Host $final_message -ForegroundColor $color
+    Out-File -FilePath $uninstall_log_path -Append -InputObject "$final_message"
+}
+function LogDebug { Log @args "DEBUG" "DarkGray" }
+function LogInfo { Log @args "INFO" "White" }
+function LogSucess { Log @args "SUCCESS" "Green" }
 
-echo "removing REZ_CONFIG_FILE environment variable"
-[Environment]::SetEnvironmentVariable('REZ_CONFIG_FILE', "", 'Machine')
+function Uninstall-All {
 
-echo "finished uninstalling rez"
+    $python_install = "$knots_install_path\python-rez"
+    $rez_install_path = "$knots_install_path\rez"
+    $rez_scripts_path = "$rez_full_install_path\Scripts\rez"
+
+    if (Test-Path -Path $rez_install_path) {
+        LogInfo "removing $rez_install_path ..."
+        Remove-Item $rez_install_path -Recurse -Force
+    }
+    if (Test-Path -Path $python_install) {
+        LogInfo "removing $python_install ..."
+        Remove-Item $python_install -Recurse -Force
+    }
+
+    # unset PATH environment variable if needed
+    # there is a probability to fucked up the user PATH variable so we recommend
+    # to backup the PATH variable that is echoed.
+    $current_path = [System.Environment]::GetEnvironmentVariable('PATH', 'Machine')
+    $new_path = ($current_path.Split(';') | Where-Object { $_ -ne $rez_scripts_path }) -join ';'
+    if ((-not($current_path -eq $new_path)) -and ($new_path)) {
+        LogDebug "current PATH environment variable: $current_path"
+        LogDebug "setting new PATH environment variable: $new_path"
+        # [System.Environment]::SetEnvironmentVariable('PATH', $new_path, 'Machine')
+        LogInfo "history of the PATH variable, in case of issue, can be found at $uninstall_log_path"
+    }
+
+    LogInfo "removing REZ_CONFIG_FILE environment variable"
+    [Environment]::SetEnvironmentVariable('REZ_CONFIG_FILE', "", 'Machine')
+    LogSucess "finished uninstalling Knots's rez"
+}
+
+Uninstall-All
