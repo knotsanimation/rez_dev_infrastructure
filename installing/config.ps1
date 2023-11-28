ce77266804931a678ac72e9e0facf9c15a6c9bfd
Liam Collod <lcollod@gmail.com>
2023-11-28 15:25:19 +0100

    refacto(installing): get skynet path from env var
    will probably change for convenience once we have a static drive letter assigned for it

--- installing/config.ps1
+++ installing/config.ps1
@@ -9,12 +9,17 @@ $KnotsInstallConfig = @{
     uninstall_log_path = ''
 }
 
+# TODO update when definitive
+$_knots_skynet_path = $env:KNOTS_SKYNET_PATH
+if (-not($_knots_skynet_path)) {
+    throw "missing KNOTS_SKYNET_PATH environment variable"
+}
+
 $KnotsInstallConfig.rez_version = "2.113.0"
 $KnotsInstallConfig.python_version = "3.10.11"
 $KnotsInstallConfig.knots_install_path = "C:\Program Files\knots"
 $KnotsInstallConfig.python_install = "$($KnotsInstallConfig.knots_install_path)\python-rez"
 $KnotsInstallConfig.rez_full_install_path = "$($KnotsInstallConfig.knots_install_path)\rez"
 $KnotsInstallConfig.rez_scripts = "$($KnotsInstallConfig.rez_full_install_path)\Scripts\rez"
-# TODO update when definitive
-$KnotsInstallConfig.rez_config_file = "N:\skynet\apps\rez\config\.rezconfig"
+$KnotsInstallConfig.rez_config_file = "$_knots_skynet_path\apps\rez\config\.rezconfig"
 $KnotsInstallConfig.uninstall_log_path = "$($KnotsInstallConfig.knots_install_path)\user-uninstall.log"
