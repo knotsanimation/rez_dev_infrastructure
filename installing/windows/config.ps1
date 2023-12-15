8d2dfae52a0911e4047afdb6fa84afa30578c79b
Liam Collod <lcollod@gmail.com>
2023-12-15 21:15:45 +0100

    !refacto(installing): config: change installation dir
    now in AppData\Local, in the effort of removing administrator privileges required for installation.

--- installing/windows/config.ps1
+++ installing/windows/config.ps1
@@ -20,11 +20,12 @@ if (-not($_knots_skynet_path)) {
 
 $KnotsInstallConfig.rez_version = "2.113.0"
 $KnotsInstallConfig.python_version = "3.10.11"
-$KnotsInstallConfig.knots_install_path = "C:\Program Files\knots"
+$KnotsInstallConfig.knots_install_path = "$env:LOCALAPPDATA\knots"
 $KnotsInstallConfig.python_install = "$($KnotsInstallConfig.knots_install_path)\python-rez"
 $KnotsInstallConfig.rez_full_install_path = "$($KnotsInstallConfig.knots_install_path)\rez"
 $KnotsInstallConfig.rez_scripts = "$($KnotsInstallConfig.rez_full_install_path)\Scripts\rez"
 $KnotsInstallConfig.rez_config_file = "$_knots_skynet_path\apps\rez\config\rezconfig-main.yml"
 $KnotsInstallConfig.rez_cache_path = "$HOME\rez\.cache"
 $KnotsInstallConfig.uninstall_log_path = "$($KnotsInstallConfig.knots_install_path)\user-uninstall.log"
+# warning: anything else might require admin permissions
 $KnotsInstallConfig.env_var_scope = "User"
