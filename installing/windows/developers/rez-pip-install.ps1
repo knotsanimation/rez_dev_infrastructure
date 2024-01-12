3569261c45afa097d50881e3b26573780bbd80a0
Liam Collod <lcollod@gmail.com>
2024-01-12 12:35:09 +0100

    refacto(installing): remove admin perm to rez-pip install

--- installing/windows/developers/rez-pip-install.ps1
+++ installing/windows/developers/rez-pip-install.ps1
@@ -1,11 +1,3 @@
-# check has admin privileges
-$sys_current_role = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
-if (
-!($sys_current_role).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-) {
-    throw "Please restart the script in a shell with Administrator permissions."
-}
-
 $rez_install = (Get-Command rez).Path
 $venv_dir = Split-Path(Split-Path $rez_install -Parent) -Parent
 # activate rez virtual environment
