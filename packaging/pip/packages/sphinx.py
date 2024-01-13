8d7819c6b018eba8586d122f3cf1192340e4acea
Liam Collod <lcollod@gmail.com>
2024-01-13 18:51:05 +0100

    refacto(pip): pip-install-tool: change callback signature

--- packaging/pip/packages/sphinx.py
+++ packaging/pip/packages/sphinx.py
@@ -1,13 +1,10 @@
-import logging
 import rez.package_maker
-
-LOGGER = logging.getLogger(__name__)
 
 
 def patch_cyclic_dependency(
     rez_package: rez.package_maker.PackageMaker,
     *args,
-):
+) -> bool:
     """
     Fix issue on some sphinx dependency that are required by ``sphinx`` but also require
     ``sphinx`` creating a cyclic dependency.
@@ -15,10 +12,10 @@ def patch_cyclic_dependency(
     We simply set a weak reference on all their ``requires`` to fix the issue.
     """
     if not rez_package.name.startswith("sphinx") or rez_package.name == "sphinx":
-        return
+        return False
 
-    LOGGER.info(f"patching {rez_package.name}")
     rez_package.requires = ["~" + require for require in rez_package.requires]
+    return True
 
 
 CONFIG = {
